37daf6c9ccc710943c72c024d94ccc446403f697
Carthonplein <carthonplein@gmail.com>
2026-09-02 18:57:44 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("🐟 Backend Carthon Plein en ligne !");
+});
+
+app.listen(PORT, () => {
+  console.log("Serveur démarré sur le port " + PORT);
+});
